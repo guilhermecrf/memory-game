b511c0f2de22dda1beebfaa7f4e04ceb39e2856d
Guilherme Santana <guilhermedrxs@gmail.com>
2024-06-26 17:53:10 -0300

    starting js

---
+++ js/main.js
@@ -0,0 +1,15 @@
+let frontCard = 'card_front'
+let backCard = 'card_back'
+
+let stacks = [
+    'bootstrap',
+    'css',
+    'electron',
+    'firebase',
+    'html',
+    'js',
+    'mongo',
+    'node',
+    'react',
+    'vue'
+]
